c100eee598697865c18f964e434497ad3cfe8686
Allen Winter <winter@kde.org>
2006-08-10 16:06:08 +0000

    remove libsyndication.h and put the namespace in syndication.h
    svn path=/trunk/KDE/kdepimlibs/; revision=571760

--- syndication.h
+++ syndication.h
@@ -1,25 +1,27 @@
 /*
-  This file is part of kdepimlibs.
-  Copyright (c) 2006 Allen Winter <winter@kde.org>
+ * This file is part of the syndication library
+ *
+ * Copyright (C) 2006 Frank Osterfeld <frank.osterfeld@kdemail.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Library General Public
-  License as published by the Free Software Foundation; either
-  version 2 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Library General Public License for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; see the file COPYING.LIB.  If not, write to
-  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-  Boston, MA 02110-1301, USA.
-*/
-
-#ifndef SYNDICATION_H
-#define SYNDICATION_H
+#ifndef SYNDICATION_SYNDICATION_H
+#define SYNDICATION_SYNDICATION_H
 
 #include <kdemacros.h>
 
@@ -33,4 +35,9 @@
 #define SYNDICATION_EXPORT KDE_EXPORT
 #endif
 
-#endif
+/**
+ * TODO
+ */
+namespace Syndication {}
+
+#endif // SYNDICATION_SYNDICATION_H
